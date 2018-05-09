4452d4db40d0833b7263231c3fcac8a61efcbd3a
el_programmador <hugogirard28@gmail.com>
2018-05-09 13:33:46 -0400

    Add more method in page

--- TopsyTurvyCakes/TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
+++ TopsyTurvyCakes/TopsyTurvyCakes/Pages/Admin/AddEditRecipe.cshtml.cs
@@ -10,15 +10,39 @@ namespace TopsyTurvyCakes.Pages.Admin
 {
     public class AddEditRecipeModel : PageModel
     {
+        private readonly IRecipesService _service;
+
         [FromRoute]
         public long? Id { get; set; }
 
         public bool IsNewRecipe => Id == null;
 
+        [BindProperty]
         public Recipe Recipe { get; set; }
 
-        public void OnGet()
+        public AddEditRecipeModel(IRecipesService service)
         {
+            _service = service;
+        }
+
+        public async Task OnGetAsync()
+        {            
+            Recipe = await _service.FindAsync(Id.GetValueOrDefault()) ?? new Recipe();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Recipe.Id = Id.GetValueOrDefault();
+
+            await _service.SaveAsync(Recipe);
+
+            return RedirectToPage("/Recipe", new { id = Recipe.Id });            
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await _service.DeleteAsync(Id.Value);
+            return RedirectToPage("/Index");
         }
     }
 }
